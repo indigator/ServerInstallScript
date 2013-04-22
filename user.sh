3b91946477f558bfc5366e039c4680dd0a275861
Jonathan Dekhtiar <marco.flint31@gmail.com>
2013-04-22 15:51:46 +0200

    prod
    Signed-off-by: Jonathan Dekhtiar <marco.flint31@gmail.com>

--- user.sh
+++ user.sh
@@ -1,10 +1,14 @@
 #!/bin/bash 
 
-while read -p "Nous allons ajouter un utilisateur, son nom ( - 1 to quit ) : " user do    
+while :
+do
+    read -p "Nous allons ajouter un utilisateur, son nom ( - 1 to quit ) : " user 
+    
     if [ $user -eq -1 ]
 	then
 		break
 	fi
+	
 	adduser --shell /usr/bin/lshell $user
 	echo "N'oublis pas de modifier les AllowUsers du fichiers : /etc/ssh/sshd_config"
 	sleep 2
