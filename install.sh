3242a143454a206aa21fd4c1c59568b3ee6b7a1c
Jonathan Dekhtiar <marco.flint31@gmail.com>
2013-04-22 02:25:29 +0200

    prod
    Signed-off-by: Jonathan Dekhtiar <marco.flint31@gmail.com>

--- install.sh
+++ install.sh
@@ -3,6 +3,21 @@
 
 apt-get install wget
 
+if [ -d /script ]
+then
+    rm -R /script
+fi
+
+if [ -f /backup.zip ]
+then
+    rm /backup.zip
+fi
+
+if [ -d /backup ]
+then
+    rm -R /backup
+fi
+
 mkdir /script
 cd /script
 wget https://raw.github.com/JonathanDekhtiar/ServerInstallScript/master/backup.sh
